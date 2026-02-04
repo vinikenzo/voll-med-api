1d934f91da400dd976dab0993d69cc030710c727
vinikenzo <kenzovini7@gmail.com>
2026-02-04 18:46:09 -0300

    implementação da classe service de autenticação

--- src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
+++ src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
@@ -1,4 +1,9 @@
 package med.voll.api.domain.usuario;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByLogin(String login);
 }
